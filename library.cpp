5c73716b2724f3dde9185c394b149449a20b8f0a
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-10-25 14:50:00 +0200

    tmp

--- library.cpp
+++ library.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+#include <typeinfo>
 
 template <typename T>
 void swapValue(T &a, T &b)
@@ -9,28 +11,23 @@ void swapValue(T &a, T &b)
 }
 
 template <typename T>
-int length0(T array[])
+int length(T array[])
 {
+    std::string typeFull = typeid(array).name();
+    std::string type = typeFull.substr(4, 4);
+
+    array = int(array);
     int arraySize = sizeof(array) / sizeof(array[0]);
     return arraySize;
 }
 
-template <typename T>
-int length1(T array[])
-{
-    int count = 0;
-    for (auto item : array) // iterate array or list
-    {
-        count++;
-    }
-    return count;
-}
 
 int main()
+
 {
     int items[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    std::cout  << length0(items) << std::endl;
-    std::cout << length1(items) << std::endl;
+    std::cout << typeid(items).name() << std::endl;
+    std::cout << length(items) << std::endl; //zeigt auf pointer!!
 
     return 0;
 }
