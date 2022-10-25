af6cfecad6f4ae2b82d5a7448fc5665523f9061f
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-10-25 22:32:32 +0200

    upd

--- library.cpp
+++ library.cpp
@@ -12,6 +12,7 @@ void cmd(string input)
     system(input.c_str());
 }
 
+
 void ic(string description, string variable)
 {
     string result = "textreset=$(tput sgr0) && purple=$(tput setaf 125) && green=$(tput setaf 2) && echo \"${purple}ic | ${textreset}" + description + ":${green} " + variable + " ${textreset}\"";
